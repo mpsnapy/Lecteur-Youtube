eaec37ae8d977f9fa7799209ba991ac21e349bb8
Matthieu PLAINEAU <MP@MPS-MacBook-Pro.local>
2018-09-16 16:54:18 +0200

    Commit de fin (sauf si vous voulez ajouter des améliorations

--- Lecteur Youtube/VideoController.swift
+++ Lecteur Youtube/VideoController.swift
@@ -7,18 +7,28 @@
 //
 
 import UIKit
+import WebKit
 
 class VideoController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     var chanson: Chanson?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if chanson != nil {
-            title = chanson?.titre
+            chargerVideo(chanson: chanson!)
         }
-        
+    
+    }
+    
+    func chargerVideo(chanson: Chanson) {
+        if let url = URL(string: chanson.videoUrl) {
+            let requete = URLRequest(url: url)
+            webView.load(requete)
+        }
     }
 
 }
